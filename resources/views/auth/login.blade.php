93b633f672644df4458a284b69f7ef6cf95fd2d4
Pilar Gonzalez <44037584+piloGC@users.noreply.github.com>
2020-09-28 23:09:51 -0300

    interfaz login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+{{-- @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<body>
+    <div class="container login-container">
+      <div class="row">
+        <div class="col-md-6 ads">
+          <h1><span id="fl">Laboratorio</span><span id="sl">Diseño Gráfico</span></h1>
+        </div>
+        <div class="col-md-6 login-form">
+          <div class="profile-img">
+            <h3>Bienvenido!</h3>
+          </div>
+          <h3>Iniciar Sesión</h3>
+          <p><small>Utilice sus credenciales de Intranet</small></p>
+          <form>
+            <div class="form-group">
+              <input type="text" class="form-control" name="username" placeholder="Ingrese su usuario">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="password" placeholder="Ingrese su contraseña">
+            </div>
+            <div class="form-group">
+              <button type="button" class="btn btn-primary btn-lg btn-block">Ingresar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
