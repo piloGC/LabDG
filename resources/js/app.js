2ba68b772b3ce27150273b60c151b2b06667dbb6
Nicholas Espinoza Hasler <nicholas.espinoza1501@alumnos.ubiobio.cl>
2020-10-24 20:18:38 -0300

    no descargar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
- import VueSweetAlert2 from 'vue-sweetalert2';
+import VueSweetAlert2 from 'vue-sweetalert2';
 
 require('./bootstrap');
 
@@ -39,5 +38,5 @@ Vue.component('eliminar-existencia', require('./components/EliminarExistencia.vu
  */
 
 const app = new Vue({
-    el: '#app', 
+    el: '#app',
 });
