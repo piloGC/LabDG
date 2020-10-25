3710b66cc9e1bdfcd1ab6e4f093cde39080ef4f6
Nicholas Espinoza Hasler <nicholas.espinoza1501@alumnos.ubiobio.cl>
2020-10-24 21:00:53 -0300

    prueba heroku

--- app/Http/Controllers/EquipoController.php
+++ app/Http/Controllers/EquipoController.php
@@ -78,7 +78,7 @@ class EquipoController extends Controller
         //$img = Image::make(public_path("storage/{$ruta_imagen}"))->fit(400,400);
         $image = $request->file('imagen');
         $filename = time() . '.' . $image->getClientOriginalExtension();
-        Image::make($image)->resize(300, 300)->save( public_path("storage/{$ruta_imagen}". $filename) );
+        Image::make($image)->resize(300, 300)->save( public_path('/uploads/public/' . $filename) );
         $img->save();
 
 
