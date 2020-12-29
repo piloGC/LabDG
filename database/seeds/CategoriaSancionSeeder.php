30c162e28ec1569c27bb98981e281d7b17b21073
Pilar Gonzalez <44037584+piloGC@users.noreply.github.com>
2020-12-29 03:41:43 -0300

    eliminacion de descripcion en categoria sancion

--- database/seeds/CategoriaSancionSeeder.php
+++ database/seeds/CategoriaSancionSeeder.php
@@ -15,25 +15,21 @@ class CategoriaSancionSeeder extends Seeder
         //
         DB::table('categoria_sancions')->insert([
             'nombre' => 'Fuera de Plazo',
-            'descripcion' => 'Entrega fuera de plazo',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
         DB::table('categoria_sancions')->insert([
             'nombre' => 'Daño Hardware o Software',
-            'descripcion' => 'Equipo posee daño fisico o lógico, evaluar costo y notificar al estudiante',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
         DB::table('categoria_sancions')->insert([
             'nombre' => 'Entregado por Tercero',
-            'descripcion' => 'Equipo encontrado en dependencias de la universidad',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
         DB::table('categoria_sancions')->insert([
             'nombre' => 'Robado',
-            'descripcion' => 'El estudiante debe entregar constancia de carabineros',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
